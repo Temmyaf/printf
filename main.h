dad0b532803dbb15e07f005c946f1def93f8edd2
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-07-09 20:03:05 -0700

    added main.h header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char  c);
+void _puts(char *str);
+int _write_char(char c);
+void _printf(char *, ...);
+char* convert(unsigned int, int);
+
+#endif
