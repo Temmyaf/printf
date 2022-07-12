18b1fc5ce0edbec0cd516d5874ddbfa7b12ca3e7
Afolabi Temiloluwa <105127078+Temmyaf@users.noreply.github.com>
2022-07-12 09:54:15 +0200

    main.h

--- main.h
+++ main.h
@@ -19,12 +19,12 @@ struct convert
 typedef struct convert conver_t;
 
 int _putchar(char  c);
+int _printf(char *format, ...);
 void _puts(char *str);
 int _strlen(char *str);
 int _write_char(char c);
-int _printf(char *format, ...);
 char *convert(unsigned int num, int base);
-int loop_count(char *format, conver_t list[], va_list arg);
+int loop_count(const char *format, conver_t list[], va_list arg);
 int print_num(va_list args);
 int print_unsigned_num(unsigned int n);
 int print_char(va_list);
